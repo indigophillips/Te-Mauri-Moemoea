e2591f96c52b36f018e84481183e4aeaaa52c383
stanley-roache <stan.roache@gmail.com>
2018-09-20 09:48:48 +1200

    first router for users

---
+++ server/routes/users.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+// const db = require('../db')
+
+const router = express.Router()
+
+router.post('/user', (req, res) => {
+  // create user route
+})
+
+module.exports = router
--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const path = require('path')
 const express = require('express')
 
-const thingRoutes = require('./routes/things')
+const userRoutes = require('./routes/users')
 
 const server = express()
 
 server.use(express.static(path.join(__dirname, 'public')))
 
-server.use('/api/v1/things', thingRoutes)
+server.use('/api/v1/users', userRoutes)
 
 module.exports = server
