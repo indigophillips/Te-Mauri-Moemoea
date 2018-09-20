cbd9c8d9bdd866e52922caaea6f19ce85836105b
stanley-roache <stan.roache@gmail.com>
2018-09-20 16:45:17 +1200

    dashboard component

---
+++ client/components/DashboardMain.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class DashboardMap extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  render() {
+    <div>dashboard main</div>
+  }
+}
