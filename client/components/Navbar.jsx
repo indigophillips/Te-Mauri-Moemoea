12dac27764baa4f193ee70222a4fdc78a599d7b4
sam-by-name <sam-sanders@hotmail.co.uk>
2018-09-21 12:00:20 +1200

    nav kinda positioned

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -4,10 +4,16 @@ const Navbar = () => {
   return (
     <div className='columns is-mobile navbar'>
       <div className='column'>
-        <div className='log'></div>
+        <div className='log element'>Log</div>
       </div>
-      <div className='column'>
-        <div className='toggle'></div>
+      <div className='column switch'>
+        <div className='toggle element'>
+          <label>
+            <input type="checkbox"></input>
+            <span className="lever"></span>
+          </label>
+
+        </div>
       </div>
       <div className='column'>
         <div className='avatar'></div>
