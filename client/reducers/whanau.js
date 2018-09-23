9a81603ab2ba932dfe9c1fdb3f73cb11481c2f67
stanley-roache <stan.roache@gmail.com>
2018-09-24 10:45:45 +1200

    default dream and whanau reducers setup to house sample data, added to root reducer

---
+++ client/reducers/whanau.js
@@ -0,0 +1,16 @@
+import {
+  whanau,
+  whanau_members
+} from '../../lib/sampleData'
+
+const sampleWhanau = {
+  whanau,
+  members: whanau_members
+}
+
+export default function whanauReducer (state = sampleWhanau, action) {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
