c419be4e4ab9ad9007aa9afe48cb30ace2d8db46
stanley-roache <stan.roache@gmail.com>
2018-09-24 10:18:35 +1200

    sampleData skeleton

---
+++ client/lib/sampleData.js
@@ -0,0 +1,92 @@
+export const user = {
+  id: 1,
+  gender: 'female',
+}
+
+export const personalDreams = [
+  {
+    name: 'License',
+    description: '...',
+    tasks: [
+      {
+        name:,
+        taha:,
+        description:,
+        done: true,
+        due_date: ''
+      },
+      {}
+    ]
+  },
+  {
+
+  }
+]
+
+export const whanauDreams = [
+  {
+    whanau_id: 1,
+    name: 'License',
+    description: '...',
+    tasks: [
+      {
+        name:,
+        taha:,
+        description:,
+        done: '',
+        due_date: ''
+      },
+      {}
+    ]
+  },
+]
+
+export const contributorDreams = [
+  {
+    dreamer_id: 1,
+    name: 'License',
+    description: '...',
+    tasks: [
+      {
+        name:,
+        taha:,
+        description:,
+        done: '',
+        due_date: ''
+      },
+      {}
+    ]
+  },
+]
+
+export const publicDreams = [
+  {
+    name: 'License',
+    description: '...',
+  }
+]
+
+export const whanau = [
+  {
+    id: 1,
+    name: '',
+    description: '',
+    member_ids: [
+      2,
+      3
+    ]
+  }
+]
+
+export const whanau_members = [
+  {
+    id: 2,
+    fist_name: '',
+    last_name: ''
+  },
+  {
+    id: 3,
+    fist_name: '',
+    last_name: ''
+  }
+]
