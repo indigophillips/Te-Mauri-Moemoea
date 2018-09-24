d02e554ef9da19f4a69e802c3d68e65ad6578012
sam-by-name <sam-sanders@hotmail.co.uk>
2018-09-24 16:22:36 +1200

    bargraph has name now

--- client/components/dashboard/Balance.jsx
+++ client/components/dashboard/Balance.jsx
@@ -13,9 +13,9 @@ class Balance extends React.Component {
     return (
       <Fragment>
         {this.props.tahaData &&
-        <ResponsiveContainer width='100%' height={200} maxWidth={150} >
+        <ResponsiveContainer width='100%' height='100%'>
           <BarChart data={this.props.tahaData} margin={{top: 0, right: 1, left: 1, bottom: 0}}>
-            <XAxis tickLine={false} stroke="#00000000" fontFamily="sans-serif"/>
+            <XAxis dataKey="name" fontSize='.6em' stroke='white' fontFamily="sans-serif"/>
             <YAxis tickLine={false} stroke="#00000000" fontFamily="sans-serif"/>
             <Tooltip />
             <Bar dataKey="completed" stackId="a" >
