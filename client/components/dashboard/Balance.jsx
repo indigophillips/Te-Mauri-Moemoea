454b3701146553b9d69c90d84323fc8c25ef43b4
stanley-roache <stan.roache@gmail.com>
2018-09-26 15:10:39 +1200

    trying to figure out balance styling

--- client/components/dashboard/Balance.jsx
+++ client/components/dashboard/Balance.jsx
@@ -2,10 +2,10 @@ import React, {Fragment} from 'react'
 
 import {BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer, Cell} from 'recharts'
 
+{/* <ResponsiveContainer width='100%' height='100%'> */}
 const Balance = props => (
   <Fragment>
     {props.data &&
-    <ResponsiveContainer width='100%' height='100%'>
       <BarChart data={props.data} margin={{top: 0, right: 1, left: 1, bottom: 0}}>
         <XAxis dataKey="name" fontSize='.6em' stroke='white' fontFamily="sans-serif"/>
         <YAxis tickLine={false} stroke="#00000000" fontFamily="sans-serif"/>
@@ -20,8 +20,7 @@ const Balance = props => (
             <Cell key={taha.name} fill={taha.color2} />
           ))}
         </Bar>
-      </BarChart>
-    </ResponsiveContainer>}
+      </BarChart>}
   </Fragment>
 )
 
