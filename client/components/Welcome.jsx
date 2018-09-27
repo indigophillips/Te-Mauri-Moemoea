fdb777066e2bdb2a567662da1899c418cdddb3ad
Indigo Georgiana <indigogeorgiana@gmail.com>
2018-09-28 11:18:35 +1200

    Final change

--- client/components/Welcome.jsx
+++ client/components/Welcome.jsx
@@ -7,7 +7,7 @@ const Welcome = () => {
       <div className="hero-body">       
         <div class='welcome'>
           <img class='welcome-image' src='images/logo_white.png'></img>
-          <h2>Kua tatoko ki manuka</h2>
+          <h2>Kua tatoko te manuka</h2>
           <h3>Learn to dream again</h3>
           <Link to='/register' type='button' className='button cancel-button' >Accept WERO</Link>
           <img class='globe-image' src='images/world.png'></img>
