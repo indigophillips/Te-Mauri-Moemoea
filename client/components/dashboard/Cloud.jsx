173eb6ca4855f9fe823f3a2097878d2b47a40594
sam-by-name <sam-sanders@hotmail.co.uk>
2018-09-28 10:58:26 +1200

    clouds move slowly in all directions

--- client/components/dashboard/Cloud.jsx
+++ client/components/dashboard/Cloud.jsx
@@ -3,23 +3,34 @@ import React, {Component} from 'react'
 class Cloud extends Component {
   constructor (props) {
     super(props)
-
     this.state = {
-      t: Math.random() * 100,
-      timer: null
+      t: Math.random() * 1000,
+      timer: null,
+      cloudType: Math.floor(Math.random()*6 + 1)
     }
   }
 
   componentDidMount () {
     this.setState({
-      timer: setInterval(() => {this.setState({t: this.state.t + 1})}, 100)
+      timer: setInterval(() => {this.setState({t: (this.state.t + 1) % 360000})}, 25)
     })
+  }
+
+  radiansToDegrees(num) {
+    return num*180/(Math.PI)
+  }
+
+  degreesToRadians(num) {
+    return num*(Math.PI)/180    
   }
 
   render () {
     return (
-      <div className='cloud column is-4' style={{top: `${Math.floor(Math.sin(this.state.t / 10)  * 30)}px`}} onClick={this.props.onClick}>
-        <img src={`./images/clouds/cloud${Math.floor(Math.random()*6 + 1)}.png`}/>
+      <div className='cloud column is-4' style={{
+        top: `${Math.floor(Math.sin(this.state.t / 140)  * 20)}px`,
+        left: `${Math.floor(Math.cos(this.state.t / 60)  * 20)}px`}}
+        onClick={this.props.onClick}>
+        <img src={`./images/clouds/cloud${this.state.cloudType}.png`}/>
         <div className='cloudtext'>
           {this.props.dream.name}
         </div>
